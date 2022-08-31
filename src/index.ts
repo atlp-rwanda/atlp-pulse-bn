c9e75db43ed124efd524db97931f5fc45bbb2d38
Fabrice Hafashimana <28519395+feyton@users.noreply.github.com>
2022-08-31 09:50:11 +0200

    Feat: Deployment setup (#22)
    - Add heroku setup

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import { ApolloServer, gql } from 'apollo-server'
-import { connect } from './database/db.config'
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
-import typeDefs from './schema/index'
+import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { connect } from "./database/db.config";
+import typeDefs from "./schema/index";
 
-import resolvers from './resolvers/resolver'
-import { context } from './context'
+import { context } from "./context";
+import resolvers from "./resolvers/resolver";
 
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,
-    plugins: [ApolloServerPluginLandingPageLocalDefault],
-    cache: 'bounded',
-    context: context
-})
+  typeDefs,
+  resolvers,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault],
+  cache: "bounded",
+  context: context,
+});
 
 connect().then(() => {
-    console.log('Database connected')
-    server.listen().then(({ url }) => console.log(`Server ready at ${url}`))
-})
+  console.log("Database connected");
+  server
+    .listen({ port: PORT })
+    .then(({ url }) => console.log(`Server ready at ${url}`));
+});
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,55 +1,66 @@
-import mongoose, { model, Schema } from 'mongoose'
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import mongoose, { model, Schema } from "mongoose";
+
+mongoose.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'user',
-    },
-    profile: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Profile',
-    },
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  profile: {
+    type: mongoose.Types.ObjectId,
+    ref: "Profile",
+  },
+});
 
-userSchema.methods.checkPass = async function (password:string) {
-    const pass = await bcrypt.compare(password, this.password)
-    return pass
-}
+userSchema.methods.checkPass = async function (password: string) {
+  const pass = await bcrypt.compare(password, this.password);
+  return pass;
+};
+
+const profileSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+});
+
+profileSchema.virtual("name").get(function () {
+  return this.firstName + " " + this.lastName;
+});
 
 
- const User = model("User", userSchema)
 
-const Profile = mongoose.model(
-    'Profile',
-    new Schema({
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-        address: {
-            type: String,
-        },
-        user: {
-            type: mongoose.Types.ObjectId,
-            ref: 'User',
-            required: true,
-            unique: true,
-        },
-    })
-)
+const User = model("User", userSchema);
+const Profile = mongoose.model("Profile", profileSchema);
 
-export { User, Profile }
+export { User, Profile };
