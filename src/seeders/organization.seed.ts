d982cc0c0afd9a6b49a91835204e84c7b1432caa
PatrickTUy <95919588+PatrickTUy@users.noreply.github.com>
2022-10-17 13:12:19 +0200

    feat(DP-123):Create backend review apps (#38) (#40)
    - create review app for opened, re-opened and synched PR
    - update the app if there're new changes added to the PR
    - close app if PR is merged or closed
    
    [#Delivers DP-123]
    
    added profile fields on signup
    
    fixed profile issues
    
    Co-authored-by: Prophete ISINGIZWE <75941936+Procech20@users.noreply.github.com>

--- src/seeders/organization.seed.ts
+++ src/seeders/organization.seed.ts
@@ -4,19 +4,19 @@ import { Organization, User } from '../models/user';
 const seedOrganizations = async () => {
   const admin = await User.find({ role: 'admin' });
 
-  const organizations = [
-    {
-      name: 'andela',
-      description: 'organization 1 description',
-      admin: admin[0]._id,
-    },
-    {
-      name: 'organization 2',
-      description: 'organization 2 description',
-      admin: admin[1]._id,
-    },
-  ];
-  await Organization.deleteMany({});
+	const organizations = [
+		{
+			name: 'Andela',
+			description: 'Master the professional and technical skills needed to accelerate your career and use technology to change the world.',
+			admin: admin[0]._id,
+		},
+		{
+			name: 'organization 2',
+			description: 'Organization 2 description',
+			admin: admin[1]._id,
+		},
+	];
+	await Organization.deleteMany({});
 
   await Organization.insertMany(organizations);
   return null;
