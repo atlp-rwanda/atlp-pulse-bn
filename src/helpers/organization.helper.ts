6813efe1f48b087649b533f7bc7363d9fca1b3c0
Alain Tresor <55650162+alaintresor@users.noreply.github.com>
2022-10-28 13:26:39 +0200

    ft(display): Display trainee in cohort (#50)
    - create getCohortTrainees query
    
    [work in progress]

--- src/helpers/organization.helper.ts
+++ src/helpers/organization.helper.ts
@@ -7,6 +7,8 @@ export async function checkLoggedInOrganization(token?: string) {
   const SECRET = process.env.SECRET || 'test_secret';
 
   if (!token) {
+    console.log(token, 'token');
+
     throw new AuthenticationError('Not logged in an organization');
   }
 
