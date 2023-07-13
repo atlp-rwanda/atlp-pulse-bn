96536755f1a9d5ea03e7555ec2195678b04665db
Habihirwe <habiholivier10@gmail.com>
2023-07-14 00:52:12 +0200

    added features on profile
    bg: fix adding member to a team

--- src/seeders/programs.seed.ts
+++ src/seeders/programs.seed.ts
@@ -1,32 +1,19 @@
-import Program from '../models/program.model';
-import { Organization, User } from '../models/user';
+import Program from '../models/program.model'
+import { Organization, User } from '../models/user'
 
 const seedPrograms = async () => {
   const andelaManagers = await User.find({
     role: 'manager',
     organizations: { $in: ['Andela'] },
-  });
+  })
 
   const IremboManagers = await User.find({
     role: 'manager',
     organizations: { $in: ['Irembo'] },
-  });
+  })
 
-  const KLabManagers = await User.find({
-    role: 'manager',
-    organizations: { $in: ['KLab'] },
-  });
-
-  const BkManagers = await User.find({
-    role: 'manager',
-    organizations: { $in: ['BK'] },
-  });
-
-  const andelaOrg = await Organization.find({ name: 'Andela' });
-  const iremboOrg = await Organization.find({ name: 'Irembo' });
-  const KlabOrg = await Organization.find({ name: 'KLab' });
-  const BkOrg = await Organization.find({ name: 'BK' });
-
+  const andelaOrg = await Organization.find({ name: 'Andela' })
+  const iremboOrg = await Organization.find({ name: 'Irembo' })
   const programs = [
     // Andela
     {
@@ -38,13 +25,7 @@ const seedPrograms = async () => {
     {
       name: 'Atlp 2',
       description: 'none',
-      manager: andelaManagers[1]._id.toHexString(),
-      organization: andelaOrg[0]._id.toHexString(),
-    },
-    {
-      name: 'Atlp 3',
-      description: 'This belong to cohort 3',
-      manager: andelaManagers[2]._id.toHexString(),
+      manager: andelaManagers[0]._id.toHexString(),
       organization: andelaOrg[0]._id.toHexString(),
     },
 
@@ -59,43 +40,15 @@ const seedPrograms = async () => {
     {
       name: 'Rwema',
       description: 'none',
-      manager: IremboManagers[1]._id.toHexString(),
+      manager: IremboManagers[0]._id.toHexString(),
       organization: iremboOrg[0]._id.toHexString(),
     },
+  ]
 
-    // BK
-    {
-      name: 'program 1 BK',
-      description: 'this is program at Irembo',
-      manager: BkManagers[0]._id.toHexString(),
-      organization: BkOrg[0]._id.toHexString(),
-    },
-    {
-      name: 'program 2 BK',
-      description: 'this is second program at BK',
-      manager: BkManagers[1]._id.toHexString(),
-      organization: BkOrg[0]._id.toHexString(),
-    },
+  await Program.deleteMany({})
 
-    // KLab
-    {
-      name: 'Robotics',
-      description: 'this is Klab program',
-      manager: KLabManagers[0]._id.toHexString(),
-      organization: KlabOrg[0]._id.toHexString(),
-    },
-    {
-      name: 'IoT',
-      description: 'this is program at Klab',
-      manager: KLabManagers[1]._id.toHexString(),
-      organization: KlabOrg[0]._id.toHexString(),
-    },
-  ];
+  await Program.insertMany(programs)
+  return null
+}
 
-  await Program.deleteMany({});
-
-  await Program.insertMany(programs);
-  return null;
-};
-
-export default seedPrograms;
+export default seedPrograms
