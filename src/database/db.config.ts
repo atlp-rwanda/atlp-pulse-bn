a4165b26c3ef7a88c8ead1becd02a437c3bbdc3c
Eric Tuyizere Ndungutse <dav.ndungutse@gmail.com>
2023-08-11 15:20:25 +0300

    feat(tickets part two): handle replies (#108)
    - ensure admin and user can reply on tickets
     - mark ticket as admin-reply, when superAdmin replies
     - mark ticket as customer-reply, when customer replies
    
    [Delivers #39-ticket-system-continued]
    
    feat(ticket): close ticket
    
     - allow users to close ticket
     - change ticket status to closed
    
    [Delivers #39-ticket-system-continued]
    
    feat(tickets): manage tickets
    
     - ensure users can see tickets they made
     - get all tickets for superAdmin
     - get all tickets created by loggedin user
    
    [Delivers #39-ticket-system-continued]

--- src/database/db.config.ts
+++ src/database/db.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import 'dotenv/config'
+import mongoose from 'mongoose'
 
 // add your own uri below
 const uri =
@@ -9,15 +9,15 @@ const uri =
     ? process.env.MONGO_PROD_DB
     : process.env.NODE_ENV === 'test'
     ? process.env.MONGO_TEST_DB
-    : process.env.MONGO_DEV_DB;
+    : process.env.MONGO_DEV_DB
 
 export const connect = async () => {
   try {
     //MONGODB CONNECTION
-    console.log('database',uri)
-    return mongoose.connect(uri!);
+    console.log('database', uri)
+    return mongoose.connect(uri!)
   } catch (error) {
-    console.log(`Database connection error: ${error}`);
-    process.exit(1);
+    console.log(`Database connection error: ${error}`)
+    process.exit(1)
   }
-};
+}
