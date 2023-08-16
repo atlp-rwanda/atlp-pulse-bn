542081d5b86f90ed05e5b8cab702c9ee8087b936
Eric Tuyizere Ndungutse <dav.ndungutse@gmail.com>
2023-08-16 21:47:59 +0300

    remove unused orginazations in seeds (#134)

--- src/seeders/organization.seed.ts
+++ src/seeders/organization.seed.ts
@@ -1,26 +1,16 @@
 /* eslint-disable */
-import { Organization, User } from '../models/user';
+import { Organization, User } from '../models/user'
 
 const seedOrganizations = async () => {
   const andelaAdmins = await User.find({
     role: 'admin',
     organizations: { $in: ['Andela'] },
-  });
+  })
 
   const IremboAdmins = await User.find({
     role: 'admin',
     organizations: { $in: ['Irembo'] },
-  });
-
-  const kLabAdmins = await User.find({
-    role: 'admin',
-    organizations: { $in: ['KLab'] },
-  });
-
-  const BkAdmins = await User.find({
-    role: 'admin',
-    organizations: { $in: ['BK'] },
-  });
+  })
 
   const organizations = [
     {
@@ -29,31 +19,17 @@ const seedOrganizations = async () => {
         'Master the professional and technical skills needed to accelerate your career and use technology to change the world.',
       admin: [...andelaAdmins.map((admin) => admin._id.toHexString())],
       gitHubOrganisation: 'atlp-rwanda',
-      activeRepos: [
-        'atlp-pulse-bn',
-        'atlp-pulse-fn'
-      ],
+      activeRepos: ['atlp-pulse-bn', 'atlp-pulse-fn'],
     },
     {
       name: 'Irembo',
       description: 'Organization 2 description',
       admin: [...IremboAdmins.map((admin) => admin._id.toHexString())],
     },
-    {
-      name: 'KLab',
-      description: 'Organization 3 description for testing',
-      admin: [...kLabAdmins.map((admin) => admin._id.toHexString())],
-    },
+  ]
 
-    {
-      name: 'BK',
-      description: 'Brainly description for testing',
-      admin: [...BkAdmins.map((admin) => admin._id.toHexString())],
-    },
-  ];
-
-  await Organization.deleteMany({});
-  await Organization.insertMany(organizations);
-  return null;
-};
-export default seedOrganizations;
+  await Organization.deleteMany({})
+  await Organization.insertMany(organizations)
+  return null
+}
+export default seedOrganizations
