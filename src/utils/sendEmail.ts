a8638704650e0d9c3761bdb689bd60618a8e52e7
Olivier <89464364+oliviertech212@users.noreply.github.com>
2023-08-17 10:51:48 +0200

    fix send email with gmail

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -10,9 +10,9 @@ export const sendEmail = async (
   senderPassword: any
 ): Promise<any> => {
   const transport = nodemailer.createTransport({
-    host: 'mail.privateemail.com',
-    port: 587,
-    secure: false,
+    host: 'smtp.gmail.com', 
+    port: 465,
+    secure: true,
     auth: {
       user: senderEmail,
       pass: senderPassword,
