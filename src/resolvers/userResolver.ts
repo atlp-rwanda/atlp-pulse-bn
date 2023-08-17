481e631a9a37ce72cef2286634e1a8af4a5ac445
Renzaho Emmanuel <71966667+teerenzo@users.noreply.github.com>
2023-08-17 12:58:12 +0200

    docs implementation (#127)

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -86,7 +86,8 @@ const resolvers: any = {
       { organisation, username }: any,
       context: Context
     ) {
-      ;(await checkUserLoggedIn(context))([
+      
+      (await checkUserLoggedIn(context))([
         'admin',
         'coordinator',
         'trainee',
