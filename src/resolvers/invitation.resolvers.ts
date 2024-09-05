e355d5917cc1b833da11942917b142c9260991e8
k3lly003 <ntirukelly@gmail.com>
2024-09-05 14:42:21 +0200

    ft(statistics): add statistics for invitation #433

--- src/resolvers/invitation.resolvers.ts
+++ src/resolvers/invitation.resolvers.ts
@@ -1,60 +1,131 @@
-import { ApolloError } from 'apollo-server';
-import { Invitation } from '../models/invitation.model';
-import { IResolvers } from '@graphql-tools/utils';
-import { checkUserLoggedIn } from '../helpers/user.helpers';
-import { checkLoggedInOrganization } from '../helpers/organization.helper';
-import Template from '../utils/templates/inviteUserTemplate';
-import { sendEmail } from '../utils/sendEmail';
-import  jwt  from 'jsonwebtoken';
-import { User } from '../models/user';
-import inviteUserTemplate from '../utils/templates/inviteUserTemplate';
-
+import { ApolloError } from 'apollo-server'
+import { Invitation } from '../models/invitation.model'
+import { IResolvers } from '@graphql-tools/utils'
+import { checkUserLoggedIn } from '../helpers/user.helpers'
+import { checkLoggedInOrganization } from '../helpers/organization.helper'
+import Template from '../utils/templates/inviteUserTemplate'
+import { sendEmail } from '../utils/sendEmail'
+import jwt from 'jsonwebtoken'
+import { User } from '../models/user'
+import inviteUserTemplate from '../utils/templates/inviteUserTemplate'
 
 const SECRET: string = process.env.SECRET ?? 'test_secret'
 const invitationResolvers: IResolvers = {
+  Query: {
+    async getAcceptedInvitations(_: any) {
+      try {
+        const invitations = await Invitation.find({ accepted: true })
+        if (!invitations.length) {
+          return []
+        }
+
+        return invitations
+      } catch (error) {
+        throw new ApolloError(
+          'Failed to fetch invitations',
+          'INTERNAL_SERVER_ERROR',
+          { error }
+        )
+      }
+    },
+    async getAcceptedInvitationsCount(_: any) {
+      try {
+        const invitations = await Invitation.find()
+        if (!invitations.length) {
+          return 0
+        }
+
+        const acceptedInvitationsCount = await User.countDocuments({
+          invitationId: { $in: invitations.map((inv) => inv._id) },
+        })
+
+        return acceptedInvitationsCount
+      } catch (error) {
+        throw new ApolloError(
+          'Failed to fetch invitations',
+          'INTERNAL_SERVER_ERROR',
+          { error }
+        )
+      }
+    },
+    async getAcceptedInvitationsPercentsCount(_: any) {
+      try {
+        const invitations = await Invitation.find()
+        if (!invitations.length) {
+          return 0
+        }
+
+        const acceptedInvitationsCount = await User.countDocuments({
+          invitationId: { $in: invitations.map((inv) => inv._id) },
+        })
+
+        const percentage = (acceptedInvitationsCount / invitations.length) * 100
+        return percentage
+      } catch (error) {
+        throw new ApolloError(
+          'Failed to calculate invitation acceptance percentage',
+          'INTERNAL_SERVER_ERROR',
+          { error }
+        )
+      }
+    },
+  },
   Mutation: {
     sendInvitation: async (
       _,
-      { invitees, orgToken,type}: { invitees: { email: string; role: string }[]; orgToken: string;type:any},
+      {
+        invitees,
+        orgToken,
+        type,
+      }: {
+        invitees: { email: string; role: string }[]
+        orgToken: string
+        type: any
+      },
       context
     ) => {
       try {
-        const { userId } =(await checkUserLoggedIn(context))(['admin']);
+        const { userId } = (await checkUserLoggedIn(context))(['admin'])
         if (!userId) {
-          throw new ApolloError('User is not logged in', 'UNAUTHENTICATED');
+          throw new ApolloError('User is not logged in', 'UNAUTHENTICATED')
         }
 
-        const org = await checkLoggedInOrganization(orgToken);
+        const org = await checkLoggedInOrganization(orgToken)
         if (!org) {
-          throw new ApolloError('Invalid organization token', 'FORBIDDEN');
+          throw new ApolloError('Invalid organization token', 'FORBIDDEN')
         }
-        const email = invitees.map(invitee => invitee?.email);
-        const userExists: any = await User.findOne(
-          { email });
+        const email = invitees.map((invitee) => invitee?.email)
+        const userExists: any = await User.findOne({ email })
 
         if (userExists) {
           throw new Error(`This user already exists in ${org.name}`)
         } else {
           const token: any = jwt.sign(
-            { name: org.name, email:invitees.map(invitee => invitee.email),
-              role:invitees.map(invitee => invitee.role )}, SECRET, {
+            {
+              name: org.name,
+              email: invitees.map((invitee) => invitee.email),
+              role: invitees.map((invitee) => invitee.role),
+            },
+            SECRET,
+            {
               expiresIn: '2d',
-            })
-        
+            }
+          )
+
           const newToken: any = token.replace(/\./g, '*')
           const newInvitation = new Invitation({
-            inviterId:userId.toString(),
-            invitees: invitees.map(invitee => ({
+            inviterId: userId.toString(),
+            invitees: invitees.map((invitee) => ({
               email: invitee.email,
-              role: invitee.role
+              role: invitee.role,
             })),
-            orgToken
-          });
-        
+            orgToken,
+          })
+
           const link =
-          type == 'user'
-            ? `${process.env.REGISTER_FRONTEND_URL}/${newToken}`
-            : `${process.env.REGISTER_ORG_FRONTEND_URL}/${newToken}`
+            type == 'user'
+              ? `${process.env.REGISTER_FRONTEND_URL}/${newToken}`
+              : `${process.env.REGISTER_ORG_FRONTEND_URL}/${newToken}`
           const content = inviteUserTemplate(org?.name || '', link)
           const someSpace = process.env.FRONTEND_LINK + 'register'
           await sendEmail(
@@ -65,15 +136,15 @@ const invitationResolvers: IResolvers = {
             process.env.ADMIN_EMAIL,
             process.env.ADMIN_PASS
           )
-        
-          await newInvitation.save();
-          return newInvitation;
+
+          await newInvitation.save()
+          return newInvitation
         }
-      } catch (error:any) {
-        throw new ApolloError(error.message, 'INTERNAL_SERVER_ERROR');
+      } catch (error: any) {
+        throw new ApolloError(error.message, 'INTERNAL_SERVER_ERROR')
       }
     },
   },
-};
+}
 
-export default invitationResolvers;
+export default invitationResolvers
--- src/resolvers/staticResolver.ts
+++ src/resolvers/staticResolver.ts
@@ -1,25 +1,12 @@
-import { Invitation } from '../models/invitation.model';
-import { Context } from '../context';
-import { checkUserLoggedIn } from '../helpers/user.helpers';
-import { checkLoggedInOrganization } from '../helpers/organization.helper';
-import { ApolloError } from 'apollo-server';
-import { User } from '../models/user';
-import { timeFilterMiddleware } from '../utils/timeFilter';
+import { Invitation } from '../models/invitation.model'
+import { ApolloError } from 'apollo-server'
 
 const StatisticsResolvers = {
   Query: {
-    getInvitationStatistics: async (
-      _: any,
-      { orgToken, timeRange }: any,
-      context: Context
-    ) => {
-      const org = await checkLoggedInOrganization(orgToken);
-      await (await checkUserLoggedIn(context))(['admin']);
+    getInvitationStatistics: async () => {
+      const invitations = await Invitation.find()
 
-      const filter = timeFilterMiddleware({ query: { timeRange } });
       try {
-        const invitations = await Invitation.find({ organization: org, ...filter.timeFilter });
-
         if (!invitations.length) {
           return {
             totalInvitations: 0,
@@ -27,41 +14,41 @@ const StatisticsResolvers = {
             pendingInvitationsCount: 0,
             getAcceptedInvitationsPercentsCount: 0,
             getPendingInvitationsPercentsCount: 0,
-          };
+          }
         }
 
         // CALCULATE TOTAL INVITATION
-        const totalInvitations = invitations.length;
+        const totalInvitations = invitations.length
 
         // CALCULATE ACCEPTED INVITATION COUNT
-        const acceptedInvitationsCount = await User.countDocuments({
-          invitationId: { $in: invitations.map((inv) => inv._id) },
-          status: 'accepted',
-        });
+        const acceptedInvitationsCount = await invitations.filter(
+          (inv) => inv.status == 'accepted'
+        ).length
 
         // CALCULATE PENDING INVITATION COUNT
-        const pendingInvitationsCount = await User.countDocuments({
-          invitationId: { $in: invitations.map((inv) => inv._id) },
-          status: 'pending',
-        });
+        const pendingInvitationsCount = await invitations.filter(
+          (inv) => inv.status == 'pending'
+        ).length
 
         return {
           totalInvitations,
           acceptedInvitationsCount,
           pendingInvitationsCount,
-          getAcceptedInvitationsPercentsCount: () => (acceptedInvitationsCount / totalInvitations) * 100,
-          getPendingInvitationsPercentsCount: () => (pendingInvitationsCount / totalInvitations) * 100,
-        };
+          getAcceptedInvitationsPercentsCount: () =>
+            (acceptedInvitationsCount / totalInvitations) * 100,
+          getPendingInvitationsPercentsCount: () =>
+            (pendingInvitationsCount / totalInvitations) * 100,
+        }
       } catch (error) {
         throw new ApolloError(
           'Failed to fetch invitation statistics',
           'INTERNAL_SERVER_ERROR',
           { error }
-        );
+        )
       }
     },
   },
   Mutation: {},
-};
+}
 
-export default StatisticsResolvers;
+export default StatisticsResolvers
