da956bc96a9a77a18b02f38acdd267b13e65f350
munezeromicha <munezerontaganiramichel@gmail.com>
2024-10-02 18:03:13 +0200

    ft-mobile-reset-password (#332)

--- src/utils/templates/forgotPasswordTemplate.ts
+++ src/utils/templates/forgotPasswordTemplate.ts
@@ -1,4 +1,4 @@
-export default function Template(link: any) {
+export default function Template(link: any, deepLink: string) {
   return /* html */ `
     <table style="font-size: 16px; font-family: 'Rubik'; text-align: left">
       <tbody>
@@ -25,9 +25,29 @@ export default function Template(link: any) {
                   color: whitesmoke;
                 "
               >
-                Reset Password
+                Reset Password (web)
               </button>
             </a>
+            <br />
+            <br />
+            <a href="${deepLink}" style="text-decoration: none; cursor: pointer">
+              <button
+                style="
+                  font-size: 16px;
+                  background-color: rgb(134, 103, 242);
+                  font-family: 'Rubik';
+                  text-align: center;
+                  border: none;
+                  border-radius: 3px;
+                  padding: 5px;
+                  cursor: pointer;
+                  color: whitesmoke;
+                "
+              >
+                Reset Password (Mobile)
+              </button>
+            </a>
+
             <br />
             <br />
 
